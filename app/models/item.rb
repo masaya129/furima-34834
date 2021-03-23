6c3f20ce1dee80812bab8e7b94042724bb7da5cd
masaya129 <mirapani666@gmail.com>
2021-03-23 14:54:11 +0900

    出品機能修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :name,    presence: true
+  validates :concept,  presence: true
+  validates :category_id, presence: true
+  validates :delivery_price_id, presence: true
+  validates :delivery_area_id, presence: true
+  validates :delivery_date_id, presence: true
+  validates :price,             presence: true
+  validates :image,             presence: true
 end
