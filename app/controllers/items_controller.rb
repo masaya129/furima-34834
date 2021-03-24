706a79acd169b8bad633e84a7bb19e8037eca0c2
masaya129 <mirapani666@gmail.com>
2021-03-24 17:19:09 +0900

    編集機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
   
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -10,24 +10,40 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
- def create
-  @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
+  def create
+    @item = Item.new(item_params)
+     if @item.save
+        redirect_to root_path
+     else
+        render :new
+     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    unless current_user.id == @item.user.id
+      redirect_to action: :index
     end
- end
+  end
 
- def show
-  @item = Item.find(params[:id])
- end
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+      unless @item.update(item_params)
+        render :edit
+      end
+  end
+  private
 
- private
+  def item_params
+    params.require(:item).permit(:name, :concept, :image, :category_id, :item_status_id, :delivery_price_id, :delivery_area_id, :delivery_date_id, :price).merge(user_id: current_user.id)
+  end
 
-def item_params
-  params.require(:item).permit(:name, :concept, :image, :category_id, :item_status_id, :delivery_price_id, :delivery_area_id, :delivery_date_id, :price).merge(user_id: current_user.id)
-end
-
-
+  def user_params
+    params.require(:user).permit(:name, :concept, :category_id, :item_status_id, :delivery_price_id, :delivery_area_id, :delivery_date_id, :price)
+  end
 end
